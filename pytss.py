f5e81c0b3279fbb37d0479bd2f7252f9a89e27f4
sankalpshukla700-art <sankalpshukla700@gmail.com>
2025-08-08 13:28:40 +0530

    Create pytss.py

---
+++ pytss.py
@@ -0,0 +1,27 @@
+import pyttsx3
+engine = pyttsx3.init() 
+
+
+rate = engine.getProperty('rate')   
+print (rate)                        
+engine.setProperty('rate', 100)     
+
+# VOLUME
+volume = engine.getProperty('volume')   # getting to know current volume level (min=0 and max=1)
+print (volume)                          # printing current volume level
+engine.setProperty('volume',9.0)        # setting up volume level  between 0 and 1
+
+# VOICE
+voices = engine.getProperty('voices')       # getting details of current voice
+#engine.setProperty('voice', voices[0].id)  # changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   # changing index, changes voices. 1 for female
+
+engine.say(''' hello git this is my first code''')
+engine.say('My current speaking rate is ' + str(rate))
+engine.runAndWait()
+engine.stop()
+
+# Saving Voice to a file
+# On Linux, make sure that 'espeak-ng' is installed
+engine.save_to_file('Hello World', 'test.mp3')
+engine.runAndWait()
